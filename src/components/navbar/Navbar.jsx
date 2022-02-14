396c065fa7e94c823219346715fdcc7c7ffa135b
Rejan <r_quebral@u.pacific.edu>
2022-02-14 12:46:06 -0800

    configuring navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,6 +10,14 @@ const Navbar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <p> <a href='#home'> Home </a></p>
+          <p> <a href='#features'> Features </a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
       </div>
     </div>
   )
