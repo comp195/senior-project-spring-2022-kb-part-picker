23d815babc297f1787d40cc4c7ed7d7ae0e4861a
Rejan <r_quebral@u.pacific.edu>
2022-02-14 12:20:37 -0800

    setting up navbar and created partPicker.html web page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 import { Article, Brand, CTA, Feature, Navbar } from './components';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+import './App.css'
 
 const App = () => {
   return (
     <div className='App'>
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
-
-        <Brand />
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <Blog />
-        <Footer />
       </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Blog />
+      <Footer />
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className='gpt3__navbar-links_logo'>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
