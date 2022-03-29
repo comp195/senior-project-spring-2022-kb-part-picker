e47dff0a77889b7a188f773f7f9caafd8b3e4f0b
Rejan <r_quebral@u.pacific.edu>
2022-03-28 22:44:24 -0700

    scraping product names from cannonkeys

--- web_scraper/scraper.js
+++ web_scraper/scraper.js
@@ -1,16 +1,43 @@
 const puppeteer = require('puppeteer')
 
-async function scrapeProduct(url) {
+async function scrapeProduct(links) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(url)
+    for(let i = 0; i < links.length; i++) {
+        await page.goto(links[i])
 
-    const [el] = await page.$x('//*[@id="ProductCardImage-collection-template-6557105356967"]')
-    const src = await el.getProperty('src')
-    const srcTxt = src.jsonValue()
-
-    console.log({srcTxt})
+        const hrefs = await page.$$eval('a', as => as.map(a => a.href))
+        getSwitchInfo(hrefs)
+    }
+    
     browser.close()
 }
 
-scrapeProduct('https://novelkeys.com/collections/keyboards')
+async function getSwitchInfo(hrefs) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    const productNames = []
+    for (let i = 0; i < hrefs.length; i++) {
+        if(hrefs[i].includes('/products/')){ 
+            const xp = '//*[@id="ProductSection-product-template"]/div/div[2]/div/h1'
+            const cur = hrefs[i]
+            console.log({cur})  
+            await page.goto(cur, {timeout: 0, waitUntil: 'networkidle0'})
+            await page.waitForXPath(xp);
+            const [e] = await page.$x(xp)
+            if (e) {
+                const e_parse = await e.getProperty('textContent')
+                const productName = await e_parse.jsonValue()
+                console.log({productName})
+                if (productName.includes('Switch') && !productName.includes('Film')) {
+                    productNames.push(productName)
+                }
+            }
+        }
+    }
+    console.log({productNames})
+    browser.close()
+}
+
+const urls = ['https://novelkeys.com/collections/switches', 'https://cannonkeys.com/collections/switches/']
+scrapeProduct(urls)
