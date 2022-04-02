8c642eec906ab31c76aed1ac315463a7fe77cf1d
Rejan <r_quebral@u.pacific.edu>
2022-04-01 23:22:25 -0700

    properly aligning nav elements

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,40 +12,38 @@ const Navbar = () => {
   
   return (
     <div className="navbar">
-        <div className='navbar-links_container'>
-          <div className='home'>
-            <p><NavLink to ='/'>KB PART PICKER</NavLink></p>
-          </div>
-          <div className="navbar-links">
-        <NavLink to ='/list-maker'>
-          <div className='makelist_img'>
-            <img src={list} alt='Make List'/>
-          </div>
-          <p> MAKE LIST </p>
-        </NavLink>
-        <a href='#features'>
-          <div className='search_img'>
-            <img src={search} alt='Search' />
-          </div>
-          <p> SEARCH </p>
-        </a>
-        <NavLink to ='/login-form'>
-          <div className='user_img'>
-            <img src={user} alt='Sign In/Sign Up' />
-            <p className='login-icon'> 
-            {accountName ? (
-              <>
-                {accountName.email.split('@')[0].toUpperCase()}
-              </>
-            ) : (
-              <>
-                SIGN IN/SIGN UP
-              </>
-            )} 
-            </p>
-          </div>
-        </NavLink>
+        <div className='home'>
+          <p><NavLink to ='/'>KB PART PICKER</NavLink></p>
         </div>
+        <div className="navbar-links">
+      <NavLink to ='/list-maker'>
+        <div className='makelist_img'>
+          <img src={list} alt='Make List'/>
+          <p> MAKE LIST </p>
+        </div>
+      </NavLink>
+      <a href='#features'>
+        <div className='search_img'>
+          <img src={search} alt='Search' />
+          <p> SEARCH </p>
+        </div>
+      </a>
+      <NavLink to ='/login-form'>
+        <div className='user_img'>
+          <img src={user} alt='Sign In/Sign Up' />
+          <p className='login-icon'> 
+          {accountName ? (
+            <>
+              {accountName.email.split('@')[0].toUpperCase()}
+            </>
+          ) : (
+            <>
+              SIGN IN/SIGN UP
+            </>
+          )} 
+          </p>
+        </div>
+      </NavLink>
       </div>
     </div>
   )
