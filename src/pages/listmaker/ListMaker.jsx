96785ff99a906958aaf7a545ed2cb59bb99dd259
Rejan <r_quebral@u.pacific.edu>
2022-04-13 14:25:51 -0700

    trying to parse snapshot object to dropdown

--- src/pages/listmaker/ListMaker.jsx
+++ src/pages/listmaker/ListMaker.jsx
@@ -1,20 +1,20 @@
-import React, {useState, useEffect, useRef, useLayoutEffect} from 'react'
+import React, {useState, useRef, useLayoutEffect} from 'react'
 import {db, useAuth} from '../../firebase'
 import {set, ref, onValue} from 'firebase/database'
 import {uid} from "uid"
 
 import './listmaker.scss'
-import { getAuth } from 'firebase/auth'
 
 const ListMaker = () => {
   const [curList, setCurList] = useState("")
-  const [lists, setLists] = useState([])
+  
   const [housing, setHousing] = useState("")
   const [switches, setSwitches] = useState("")
   const [keycap, setKeycap] = useState("")
   const [pcb, setPCB] = useState("")
 
   const accountName = useAuth()
+  var lists = []
 
   // make sure account auth is checked before getting list from db
   const firstUpdate = useRef(true);
@@ -23,11 +23,7 @@ const ListMaker = () => {
       firstUpdate.current = false;
       return;
     }
-    {accountName ? (
-      getPartListFromDatabase()
-    ):(
-      handleDisabledButton()
-    )}
+    {accountName ? (getPartListFromDatabase()):(handleDisabledButton())}
   });
   
   const handleDisabledButton = () => {
@@ -37,11 +33,19 @@ const ListMaker = () => {
   // read
   async function getPartListFromDatabase() {
     const list_ref = ref(db, 'PartList/')
-    onValue(list_ref, (snapshot) => {
+    await onValue(list_ref, (snapshot) => {
       snapshot.forEach(function(childSnapshot) {
-        if (childSnapshot.child('current_uid').val() == accountName.uid)
-          var data = childSnapshot.val()
-          lists.push(data)
+        if (childSnapshot.child('current_uid').val() === accountName.uid)
+          var list = {
+            current_uid : childSnapshot.child('current_uid').val(),
+            housing : childSnapshot.child('housing').val(),
+            keycap : childSnapshot.child('keycap').val(),
+            list_id : childSnapshot.child('list_id').val(),
+            list_name : childSnapshot.child('list_name').val(),
+            pcb : childSnapshot.child('pcb').val(),
+            switches : childSnapshot.child('switches').val()
+          }
+          lists.push(list)
       })
     })
     console.log({lists})
@@ -53,6 +57,7 @@ const ListMaker = () => {
     const current_uid = accountName.uid
     set (ref(db, `PartList/${list_id}`), {
       current_uid,
+      list_id,
       list_name: lists,
       housing,
       switches,
@@ -70,8 +75,14 @@ const ListMaker = () => {
       {/* <input type="text" name='New List' required className="list-name" placeholder="New List" onChange={(e) => setList(e.target.value)} /> */}
       <select name="Lists" id="lists" value={curList} onChange={(e) => setCurList(e.target.value)} >
           <option value="default">--user lists--</option>
-          {lists.map((item) => <option key={item.key} value={item.list_name}>{item.list_name}</option>)}  
-      </select> 
+          {accountName ? (
+            <>
+            {lists.map((item, index) => <option key={item.list_id} value={item.list_name}>{item.list_name}</option>)}  
+            </>
+          ) : ( 
+            <> </>
+          )}
+            </select> 
       <div className="slide">
         <select name="Keycaps" id="keycaps" value={keycap} onChange={(e) => setKeycap(e.target.value)} >
           <option value="default">keycaps</option>
