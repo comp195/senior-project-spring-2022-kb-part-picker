5ae830393fbf9f5b9acb3f372ff626f12d728376
Rejan <r_quebral@u.pacific.edu>
2022-04-17 14:30:59 -0700

    handling toggles between choosing and editing list

--- src/pages/listmaker/ListMaker.jsx
+++ src/pages/listmaker/ListMaker.jsx
@@ -8,6 +8,8 @@ import './listmaker.scss'
 
 const ListMaker = () => {
   const [curList, setCurList] = useState("")
+  const [curListName, setCurListName] = useState('')
+  const listSelected = useRef(false)
   
   const [keycap, setKeycap] = useState("")
   const [housing, setHousing] = useState("")
@@ -21,15 +23,19 @@ const ListMaker = () => {
 
   const makeBasicPartComponent = (category) => {
     var link = '/' + category.toLowerCase()
+    
     return(
       <tr className="list-item">
         <td className="item-category">{category}</td>
         <td className="item-add-button">
-          <NavLink to ={link}>
+          {listSelected.current ? (<></>):(
+            <NavLink to ={link}>
             <button className="add-item-button">
               Add {category}
             </button>
           </NavLink>
+          )}
+          
         </td>
       </tr>
     )
@@ -40,7 +46,7 @@ const ListMaker = () => {
       <tr className="list-item">
         <td className="item-category">Case</td>
         <td className="item-image"><img src={p.img_url} alt={p.product_name}/></td>
-        <td className="item-name">{p.product_name}</td>
+        <td className="item-name"><a href="">{p.product_name}</a></td>
         {category.includes('Housing') ? (<td className="item-size">{p.size}</td>):(<></>)}
         {category.includes('Keycaps') ? (<td className="item-material">{p.material}</td>):(<></>)}
         {category.includes('Switches') ? (<td className="item-size">{p.size}</td>):(<></>)}
@@ -53,29 +59,52 @@ const ListMaker = () => {
     return 0
   }
 
+  const handleWriteList = () => {
+    return
+  }
+
+  const handleEnableEditList = () => {
+    listSelected.current = false 
+  }
+
   const handleChangeList = (e) => {
+    listSelected.current = true
     setCurList(e)
+    setCurListName(e.list_name)
 
     var item = lists[lists.findIndex(l => l.list_id === e)]
+    
     //   housing,
     if (!item.housing.includes("Unknown")) {
       var h = getPartFromDatabase('Housing/', item.housing)
       setHousing(makeSpecificPartComponent(h, 'Housing'))
+    }
+    else {
+      setHousing(makeBasicPartComponent('Housing'))
     }
     //   switches,
     if (!item.switches.includes("Unknown")) {
       var sw = getPartFromDatabase('Switches/', item.housing)
       setSwitches(makeSpecificPartComponent(sw, 'Switches'))
     }
+    else {
+      setSwitches(makeBasicPartComponent('Switches'))
+    }
     //   keycap,
     if (!item.keycap.includes("Unknown")) {
       var kc = getPartFromDatabase('Keycaps/', item.housing)
       setKeycap(makeSpecificPartComponent(kc, 'Keycaps'))
     }
+    else {
+      setKeycap(makeBasicPartComponent('Keycaps'))
+    }
     //   pcb
     if (!item.pcb.includes("Unknown")) {
       var p = getPartFromDatabase('PCB/', item.housing)
       setPCB(makeSpecificPartComponent(p, 'PCB'))
+    }
+    else {
+      setPCB(makeBasicPartComponent('PCB'))
     }
 
   }
@@ -90,7 +119,6 @@ const ListMaker = () => {
             setLists(old =>[...old, childSnapshot.val()])
         })
       })
-      console.log({lists})
       setDBUpdating(false)
     }
   }
@@ -106,18 +134,17 @@ const ListMaker = () => {
 
   // write
   const writeToDatabase = () => {
-    // const list_id = uid()
-    // const current_uid = accountName.uid
-    // set (ref(db, `PartList/${list_id}`), {
-    //   current_uid,
-    //   list_id,
-    //   list_name: lists,
-    //   housing,
-    //   switches,
-    //   keycap,
-    //   pcb
-    // })
-    console.log({lists})
+    const list_id = uid()
+    const current_uid = accountName.uid
+    set (ref(db, `PartList/${list_id}`), {
+      current_uid,
+      list_id,
+      list_name: lists,
+      housing,
+      switches,
+      keycap,
+      pcb
+    })
   }
   
   // update
@@ -132,7 +159,7 @@ const ListMaker = () => {
     }
 
     if (dbUpdating) {
-      {accountName ? (getPartListFromDatabase()):(handleNothing())}
+      {accountName ? getPartListFromDatabase():handleNothing()}
       setDBUpdating(false)
       setKeycap(makeBasicPartComponent("Keycaps"))
       setHousing(makeBasicPartComponent("Housing"))
@@ -142,16 +169,12 @@ const ListMaker = () => {
   })
 
   return (
-    
     <>
-    
       <div className="list-maker">
       {dbUpdating ? (<p>Loading...</p>):(
-
         <>
-        {/* <input type="text" name='New List' required className="list-name" placeholder="New List" onChange={(e) => setList(e.target.value)} /> */}
-        <select className='list-select' name="Lists" id="lists" value={curList} onChange={(e) => handleChangeList(e)} >
-          <option key='default' className='default' value="default">user lists</option>
+        <select className='list-select' name="Lists" id="lists" value={curList} onChange={(e) => handleChangeList(e.target.value)} >
+          <option default hidden>user lists</option>
           {dbUpdating ? (<></>): (
               lists.map((l,i) => (
                 <option key={i} value={l.list_id}>{l.list_name}</option>
@@ -159,6 +182,8 @@ const ListMaker = () => {
             )
           }  
         </select> 
+
+        {listSelected.current ? <></> : <input type="text" name='New List' required className="list-name" placeholder={curListName}  />}
         <table>
           <tbody>
             {keycap}
@@ -167,19 +192,24 @@ const ListMaker = () => {
             {pcb}
           </tbody>
         </table>
+        
         <div className="makelist-button">
           {accountName ? (
-            <button onClick={writeToDatabase}>Make List</button>
-          ) : (
-            <button disabled className='disabled' onClick={handleNothing}>Make List</button>
-          )}
+            <>
+            {listSelected.current ? 
+              <button onClick={handleEnableEditList}>Edit List</button>
+            :
+              <button onClick={handleWriteList}>Make List</button>
+            }
+            </>
+          ) : 
+            <button disabled className='disabled'>Make List</button>
+          }
         </div>
         </>
       )
       }
     </div>
-   
-    
   </>
   )
 }
